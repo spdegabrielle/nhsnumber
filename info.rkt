51813242122e598135f00cfe3c4f7ff0ee6deae3
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2014-11-17 10:45:31 +0000

    Create info.rkt

---
+++ info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+(define collection "NHS Number Validator")
+
+(define deps '("base"))
+
+(define build-deps '("rackunit-lib" "scribble-lib" "racket-doc" "typed-racket-lib"))
+
+(define version "0.2")
+
+(define primary-file "nhs-number.rkt")
+
